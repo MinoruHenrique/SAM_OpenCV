d11b7cc509a8b3b8bf5baaeed144c1f75ceecd1e
MinoruHenrique <henrique.deaguiar.m@uni.pe>
2023-05-12 14:18:27 -0500

    Add folder input functionality for segmentation

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import argparse
 import os
-from utils import show_mask, show_box, check_if_folder_exists
+from utils import show_mask, show_box, check_if_folder_exists, do_segmentation, save_masks, show_segmentation
     
     
 sam_checkpoint = "sam_vit_h_4b8939.pth"
@@ -14,71 +14,38 @@ device = "cuda"
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--image", help="Path for image to be segmented")
+    parser.add_argument("-i", "--data", help="Path for image or image folder to be segmented")
     parser.add_argument("-f", "--folder", help="Folder where dataset will be created from segmented image")
     args = parser.parse_args()
     
     dest_folder = args.folder
+    check_if_folder_exists(dest_folder)
     
     sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
     sam.to(device=device)
     predictor = SamPredictor(sam)
-    # Cargar imagen
-    image = cv2.imread(args.image)
-    raw_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-    predictor.set_image(raw_image)
-
-    # Mostrar imagen
-    cv2.imshow("Imagen", image)
-
-    rects = []
-    while True:
-        # Seleccionar región con un rectángulo
-        rect = cv2.selectROI("Imagen", image, False)
-
-        # Mostrar región seleccionada con un rectángulo
-        x, y, w, h = rect
-        new_rect = [x,y,x+w,y+h]
-        rects.append(new_rect)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.imshow("Imagen", image)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                # print(rects)
-                break
-
-    input_boxes = torch.tensor(rects, device=predictor.device)
-
-    transformed_boxes = predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
-    masks, _, _ = predictor.predict_torch(
-        point_coords=None,
-        point_labels=None,
-        boxes=transformed_boxes,
-        multimask_output=False,
-    )
-    # print(masks.shape)
-
-    #Save masks
-    check_if_folder_exists(dest_folder)
-    i=len(os.listdir(os.path.join(dest_folder,"images")))
-    for mask in masks:
-        mask_as_numpy = mask.cpu().numpy()[0]
-        image_name = f"{i}.png"
-        mask_name = f"{i}_seg0.png"
-        # print(mask.cpu().numpy())
-        cv2.imwrite(os.path.join(dest_folder,"segmentations",mask_name),mask_as_numpy*255)
-        cv2.imwrite(os.path.join(dest_folder,"images",image_name), cv2.cvtColor(raw_image,cv2.COLOR_RGB2BGR))
-        i+=1
-    
-    
-    plt.figure(figsize=(10, 10))
-    plt.imshow(raw_image)
-    for mask in masks:
-        show_mask(mask.cpu().numpy(), plt.gca(), random_color=True)
-    for box in input_boxes:
-        show_box(box.cpu().numpy(), plt.gca())
-    plt.axis('off')
-    plt.show()
-    print(masks)
+    # Single photo
+    if(os.path.isfile(args.data)):
+        image = cv2.imread(args.data)
+        raw_image = image.copy()
+        masks, input_boxes = do_segmentation(predictor, image)
+        #Save masks
+        save_masks(dest_folder,raw_image,masks)
+        #Show segmentation as a matplotlib plot
+        show_segmentation(raw_image, masks, input_boxes)
+    # Folder of photos
+    elif(os.path.isdir(args.data)):
+        images = os.listdir(args.data)
+        for image in images:
+            image_path = os.path.join(args.data, image)
+            image = cv2.imread(image_path)
+            raw_image = image.copy()
+            masks, input_boxes = do_segmentation(predictor, image)
+            #Save masks
+            save_masks(dest_folder, raw_image, masks)
+            
+            #Show segmeentaiton as a matplotlib plot
+            show_segmentation(raw_image, masks, input_boxes)
+            
 
    
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import os
-
+import cv2
+import torch
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
@@ -36,3 +37,48 @@ def check_if_folder_exists(folder):
     if(not os.path.exists(segm_folder)):
         print(f"The path {segm_folder} doesn't exist. Creating {segm_folder}...")
         os.mkdir(segm_folder)
+def do_segmentation(predictor, image):
+    predictor.set_image(image)
+    cv2.imshow("Imagen", image)
+    rects = []
+    while True:
+        # Seleccionar región con un rectángulo
+        rect = cv2.selectROI("Imagen", image, False)
+        # Mostrar región seleccionada con un rectángulo
+        x, y, w, h = rect
+        new_rect = [x,y,x+w,y+h]
+        rects.append(new_rect)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.imshow("Imagen", image)
+        if cv2.waitKey(0) & 0xFF == ord('q'):                
+            cv2.destroyAllWindows()
+            # print(rects)
+            break
+    input_boxes = torch.tensor(rects, device=predictor.device)
+    transformed_boxes = predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
+    masks, _, _ = predictor.predict_torch(
+        point_coords=None,
+        point_labels=None,
+        boxes=transformed_boxes,
+        multimask_output=False,
+    )
+    return masks, input_boxes
+def save_masks(dest_folder, image, masks):
+    i=len(os.listdir(os.path.join(dest_folder,"images")))
+    for mask in masks:
+        mask_as_numpy = mask.cpu().numpy()[0]
+        image_name = f"{i}.png"
+        mask_name = f"{i}_seg0.png"
+        # print(mask.cpu().numpy())
+        cv2.imwrite(os.path.join(dest_folder,"segmentations",mask_name),mask_as_numpy*255)
+        cv2.imwrite(os.path.join(dest_folder,"images",image_name), image)
+        i+=1
+def show_segmentation(image, masks, input_boxes):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
+    for mask in masks:
+        show_mask(mask.cpu().numpy(), plt.gca(), random_color=True)
+    for box in input_boxes:
+        show_box(box.cpu().numpy(), plt.gca())
+    plt.axis('off')
+    plt.show()
