6b96cbb030ec785dd20340ff5b2c51a8062aa8bd
HF-davis <henrique.deaguiar.m@uni.pe>
2023-06-02 18:23:41 -0500

    Add point functionality

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import cv2
 from segment_anything import sam_model_registry, SamPredictor
-import torch
-from matplotlib import pyplot as plt
-import numpy as np
 import argparse
 import os
-from utils import show_mask, show_box, check_if_folder_exists, do_segmentation, save_masks, show_segmentation
+from utils import check_if_folder_exists, do_segmentation, save_masks, show_segmentation
     
-    
+_AVAILABLE_MODES = ["bb","pt"]
+
 sam_checkpoint = "sam_vit_h_4b8939.pth"
 model_type = "vit_h"
 device = "cuda"
@@ -16,23 +14,31 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--data", help="Path for image or image folder to be segmented")
     parser.add_argument("-f", "--folder", help="Folder where dataset will be created from segmented image")
+    parser.add_argument("-m", "--mode", help="Use mode (bb for bounding boxes/ pt for point selection)")
     args = parser.parse_args()
+    
+    mode = args.mode
+    
+    if mode not in _AVAILABLE_MODES:
+        raise ValueError(f"Mode '{mode}' not supported")
     
     dest_folder = args.folder
     check_if_folder_exists(dest_folder)
     
+    print("Loading SAM weights...")
     sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
     sam.to(device=device)
     predictor = SamPredictor(sam)
+   
     # Single photo
     if(os.path.isfile(args.data)):
         image = cv2.imread(args.data)
         raw_image = image.copy()
-        masks, input_boxes = do_segmentation(predictor, image)
+        masks, input_boxes = do_segmentation(predictor, image, mode)
         #Save masks
-        save_masks(dest_folder,raw_image,masks)
+        save_masks(dest_folder,raw_image,masks,mode)
         #Show segmentation as a matplotlib plot
-        show_segmentation(raw_image, masks, input_boxes)
+        show_segmentation(raw_image, masks, input_boxes, mode)
     # Folder of photos
     elif(os.path.isdir(args.data)):
         images = os.listdir(args.data)
@@ -40,12 +46,12 @@ if __name__=="__main__":
             image_path = os.path.join(args.data, image)
             image = cv2.imread(image_path)
             raw_image = image.copy()
-            masks, input_boxes = do_segmentation(predictor, image)
+            masks, input_boxes = do_segmentation(predictor, image, mode)
             #Save masks
-            save_masks(dest_folder, raw_image, masks)
+            save_masks(dest_folder, raw_image, masks,mode)
             
-            #Show segmeentaiton as a matplotlib plot
-            show_segmentation(raw_image, masks, input_boxes)
+            #Show segmentation as a matplotlib plot
+            show_segmentation(raw_image, masks, input_boxes, mode)
             
 
    
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from matplotlib import pyplot as plt
 import os
 import cv2
 import torch
+
+point = [0,0]
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
@@ -37,48 +39,102 @@ def check_if_folder_exists(folder):
     if(not os.path.exists(segm_folder)):
         print(f"The path {segm_folder} doesn't exist. Creating {segm_folder}...")
         os.mkdir(segm_folder)
-def do_segmentation(predictor, image):
+        
+
+            
+            
+def do_segmentation(predictor, image, mode):
+    global point
+    def click_callback(event, x, y, flags, param):
+        global point
+        # grab references to the global variables
+        # if the left mouse button was clicked, record the starting
+        # (x, y) coordinates and indicate that cropping is being
+        # performed
+        if event == cv2.EVENT_LBUTTONUP:
+            # record the ending (x, y) coordinates and indicate that
+            # the cropping operation is finished
+            cv2.circle(image, [x,y], 0, (0,0,200), 6)
+            point = [x, y]
+            print(point)
+        
     predictor.set_image(image)
+    print(predictor.device)
+    height, width, _ = image.shape
+    if(height>width):
+        new_height = 1300
+        new_width = int((new_height/height)*width)
+    else:
+        new_width = 1300
+        new_height = int((new_width/width)*height)
+    cv2.namedWindow("Imagen", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Imagen", new_width, new_height)
     cv2.imshow("Imagen", image)
     rects = []
-    while True:
-        # Seleccionar región con un rectángulo
-        rect = cv2.selectROI("Imagen", image, False)
-        # Mostrar región seleccionada con un rectángulo
-        x, y, w, h = rect
-        new_rect = [x,y,x+w,y+h]
-        rects.append(new_rect)
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.imshow("Imagen", image)
-        if cv2.waitKey(0) & 0xFF == ord('q'):                
-            cv2.destroyAllWindows()
-            # print(rects)
-            break
-    input_boxes = torch.tensor(rects, device=predictor.device)
-    transformed_boxes = predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
-    masks, _, _ = predictor.predict_torch(
-        point_coords=None,
-        point_labels=None,
-        boxes=transformed_boxes,
-        multimask_output=False,
-    )
-    return masks, input_boxes
-def save_masks(dest_folder, image, masks):
+    if mode == "bb":
+        while True:
+            # Seleccionar región con un rectángulo
+            rect = cv2.selectROI("Imagen", image, False)
+            # Mostrar región seleccionada con un rectángulo
+            x, y, w, h = rect
+            new_rect = [x,y,x+w,y+h]
+            rects.append(new_rect)
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.imshow("Imagen", image)
+            if cv2.waitKey(0) & 0xFF == ord('q'):                
+                cv2.destroyAllWindows()
+                # print(rects)
+                break
+        input_boxes = torch.tensor(rects, device=predictor.device)
+        transformed_boxes = predictor.transform.apply_boxes_torch(input_boxes, image.shape[:2])
+        masks, _, _ = predictor.predict_torch(
+            point_coords=None,
+            point_labels=None,
+            boxes=transformed_boxes,
+            multimask_output=False,
+        )
+        return masks, input_boxes
+    elif mode == "pt":
+        while True:
+            #Select region with a point
+            cv2.setMouseCallback("Imagen", click_callback)
+            if cv2.waitKey(0) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
+        masks, scores, logits = predictor.predict(
+            point_coords=np.array([point]),
+            point_labels=np.array([1]),
+            multimask_output=True,
+        )
+        return [masks[2]], point
+         
+
+def save_masks(dest_folder, image, masks,mode):
     i=len(os.listdir(os.path.join(dest_folder,"images")))
     for mask in masks:
-        mask_as_numpy = mask.cpu().numpy()[0]
+        if(mode=="bb"):
+            mask_as_numpy = mask.cpu().numpy()[0]
+        else:
+            mask_as_numpy = mask
         image_name = f"{i}.png"
         mask_name = f"{i}_seg0.png"
         # print(mask.cpu().numpy())
         cv2.imwrite(os.path.join(dest_folder,"segmentations",mask_name),mask_as_numpy*255)
         cv2.imwrite(os.path.join(dest_folder,"images",image_name), image)
         i+=1
-def show_segmentation(image, masks, input_boxes):
+        
+def show_segmentation(image, masks, input, mode):
     plt.figure(figsize=(10, 10))
     plt.imshow(cv2.cvtColor(image,cv2.COLOR_BGR2RGB))
     for mask in masks:
-        show_mask(mask.cpu().numpy(), plt.gca(), random_color=True)
-    for box in input_boxes:
-        show_box(box.cpu().numpy(), plt.gca())
+        if(mode=="bb"):
+            mask=mask.cpu().numpy()
+        show_mask(mask, plt.gca(), random_color=True)
+    if mode == "bb":
+        for box in input:
+            show_box(box.cpu().numpy(), plt.gca())
+    elif mode == "pt":
+        show_points(np.array([input]), np.array([1]), plt.gca())
     plt.axis('off')
     plt.show()
